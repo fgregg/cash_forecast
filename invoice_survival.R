5d825820f2db3efe7670cad6e8c563921b769a18
Forest Gregg <fgregg@uchicago.edu>
2019-12-04 20:04:28 -0600

    does overdue status affects hazard rate... no

--- invoice_survival.R
+++ invoice_survival.R
@@ -14,8 +14,17 @@ n_business_days = function(start_date, end_date) {
 }
 
 
+OVERDUE_DAYS = 30
+
 invoices = read.csv('invoice_age.csv')
+
+invoices$id = row.names(invoices)
 invoices$paid = as.logical(invoices$paid)
+
+invoices$invoice_created = as.Date(invoices$invoice_created)
+
+invoices$invoice_closed[invoices$paid == FALSE] = NA
+invoices$invoice_closed = as.Date(invoices$invoice_closed)
 
 # Bigger invoices may be take longer to get paid than smaller
 # invoices, let's capture that with a log scale
@@ -24,12 +33,12 @@ invoices$log_amount = log(invoices$amount)
 # Age in business days
 invoices$age[invoices$paid == TRUE] = mapply(
     n_business_days,
-    as.Date(invoices$invoice_created[invoices$paid == TRUE]),
-    as.Date(invoices$invoice_closed[invoices$paid == TRUE]))
+    invoices$invoice_created[invoices$paid == TRUE],
+    invoices$invoice_closed[invoices$paid == TRUE])
 
 invoices$age[invoices$paid == FALSE] = mapply(
     n_business_days,
-    as.Date(invoices$invoice_created[invoices$paid == FALSE]),
+    invoices$invoice_created[invoices$paid == FALSE],
     Sys.Date())
 
 # Exclude invoices for 0 dollars, or ones that got paid 
@@ -39,21 +48,52 @@ invoices = invoices[invoices$amount != 0,]
 
 # Get the Kaplan Meier fit
 km_fit = survival::survfit(survival::Surv(age, paid) ~ 1, data=invoices)
+
 pdf(file='km.pdf')
-plot(km_fit, xlim=c(0, 70), xlab='Business Days')
+plot(km_fit,
+     xlab='Business Days',
+     main='Proportion of Invoices Open After N Business Days, Historic')
 dev.off()
+
+invoices$age_due = mapply(
+    n_business_days,
+    invoices$invoice_created,
+    invoices$invoice_created + OVERDUE_DAYS)
+
+invoices$overdue = invoices$age > invoices$age_due
+
+timely = invoices[invoices$overdue == FALSE, ]
+timely$time_0 = 0
+timely$time_1 = timely$age
+timely$overdue = FALSE
+
+due = invoices[invoices$overdue == TRUE, ]
+due$time_0 = 0
+due$time_1 = due$age_due
+due$overdue = FALSE
+due$paid = FALSE
+
+overdue = invoices[invoices$overdue == TRUE, ]
+overdue$time_0 = overdue$age_due
+overdue$time_1 = overdue$age
+overdue$overdue = TRUE
+
+complete = rbind(timely, due, overdue)
 
 # This is or starting model. It has a single covariate, size of
 # the invoice. We can make this more sophisticated in the
 # future
-fit = survival::coxph(survival::Surv(age, paid) ~ log_amount, data=invoices)
+fit = survival::coxph(survival::Surv(time_0, time_1, paid) ~ log_amount, data=complete)
 summary(fit)
+
+fit_2 = survival::coxph(survival::Surv(age, paid) ~ log_amount, data=invoices)
+summary(fit_2)
 
 # Now we want to predict whether our current outstanding invoices
 # will be paid in the next 30 days. We first create our data frame to
 # predict off of
-next_30 = invoices[invoices$paid == FALSE,]
-next_30$age <- n_business_days(Sys.Date(), Sys.Date() + 30)
+next_30 = complete[complete$paid == FALSE & complete$time_1 == complete$age,]
+next_30$time_1 = next_30$time_0 + n_business_days(Sys.Date(), Sys.Date() + 30)
 
 # Here are the individual probabilities that a invoice will be paid
 # in the next 30 days
@@ -65,7 +105,7 @@ pr_paid = 1 - exp(-predict(fit, next_30, type='expected'))
 #
 # We simulate many outcomes and graph the resulting distribution
 runs = c()
-for (j in 1:100000) {
+for (j in 1:10000) {
     expected_cash = 0
     for (i in 1:nrow(next_30)) {
         pr = pr_paid[i]
@@ -75,8 +115,8 @@ for (j in 1:100000) {
     runs[j] = expected_cash
 }
 
-pdf(file='expected_realized_cash.pdf')
+#pdf(file='expected_realized_cash.pdf')
 hist(runs, main='Realized cash from current Accounts Receivable, 30 days')
-dev.off()
+#dev.off()
 
 summary(runs)
